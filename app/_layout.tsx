d72963c22a0645e4deaa4e48596dab3e79030c66
daniel <daniel15898@gmail.com>
2024-08-25 14:45:10 +0300

    set app expo environment

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,38 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { icons } from "@/constants";
+import fonts from "@/constants/fonts";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  const a = icons.eye;
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Black": fonts.PoppinsBlack,
+    "Poppins-Bold": fonts.PoppinsBold,
+    "Poppins-ExtraBold": fonts.PoppinsExtraBold,
+    "Poppins-ExtraLight": fonts.PoppinsExtraLight,
+    "Poppins-Light": fonts.PoppinsLight,
+    "Poppins-Medium": fonts.PoppinsMedium,
+    "Poppins-Regular": fonts.PoppinsRegular,
+    "Poppins-SemiBold": fonts.PoppinsSemiBold,
+    "Poppins-Thin": fonts.PoppinsThin,
   });
 
   useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
+    if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" />
+    </Stack>
   );
 }
---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const WelcomePage = () => {
+  return (
+    <View className="flex items-center justify-center h-full">
+      <Text>index</Text>
+    </View>
+  );
+};
+
+export default WelcomePage;
+
+const styles = StyleSheet.create({});
